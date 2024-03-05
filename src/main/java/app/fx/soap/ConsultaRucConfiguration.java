60deb0c1a930d30e1f4aade873fedebfcb5288d4
Ignacio Salcedo <salcedo.ig82@gmail.com>
2024-03-05 11:14:16 -0300

    App JavaFx SDK 17.0.10, JDK 17, SpringBoot 3 Consumo de servicio web SOAP, Implementacion Spring-ws-security 4.x Tipo autenticacion "UsernameToken"

--- src/main/java/app/fx/soap/ConsultaRucConfiguration.java
+++ src/main/java/app/fx/soap/ConsultaRucConfiguration.java
@@ -5,11 +5,32 @@ import app.fx.soap.wsdl.RucContribuyente;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
+import org.springframework.ws.transport.http.HttpComponentsMessageSender;
 
 @Configuration
 public class ConsultaRucConfiguration {
 
+    @Bean
+    public WebServiceTemplate webServiceTemplate(Jaxb2Marshaller marshaller) {
+        WebServiceTemplate webServiceTemplate = new WebServiceTemplate(marshaller);
 
+        // Configurar la seguridad
+        Wss4jSecurityInterceptor securityInterceptor = new Wss4jSecurityInterceptor();
+        securityInterceptor.setSecurementActions("UsernameToken");
+        securityInterceptor.setSecurementUsername("user"); // Nombre de usuario
+        securityInterceptor.setSecurementPassword("password"); // Contraseña
+        //securityInterceptor.setSecurementPasswordType("PasswordText"); // Tipo de contraseña
+
+        // Agregar el interceptor de seguridad al WebServiceTemplate
+        webServiceTemplate.setInterceptors(new org.springframework.ws.client.support.interceptor.ClientInterceptor[] {securityInterceptor});
+
+        // Configurar un sender HTTP para el transporte
+        //webServiceTemplate.setMessageSender(new HttpComponentsMessageSender());
+
+        return webServiceTemplate;
+    }
     @Bean
     public Jaxb2Marshaller marshaller(){
         Jaxb2Marshaller marshaller= new Jaxb2Marshaller();
@@ -18,10 +39,16 @@ public class ConsultaRucConfiguration {
     }
     @Bean
     public ConsultaRucSoapClient consultaRucSoapClient(Jaxb2Marshaller marshaller){
-        ConsultaRucSoapClient client = new ConsultaRucSoapClient();
-        client.setDefaultUri("http://localhost:8097/ws");
-        client.setMarshaller(marshaller);
-        client.setUnmarshaller(marshaller);
+        ConsultaRucSoapClient client = null;
+        try {
+            client = new ConsultaRucSoapClient();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        client.setDefaultUri("http://localhost/ws");
+//        client.setMarshaller(marshaller);
+//        client.setUnmarshaller(marshaller);
+        client.setWebServiceTemplate(webServiceTemplate(marshaller));
         return client;
     }
 }
--- src/main/java/app/fx/soap/client/ConsultaRucSoapClient.java
+++ src/main/java/app/fx/soap/client/ConsultaRucSoapClient.java
@@ -5,10 +5,35 @@ import app.fx.soap.wsdl.ConsultaRucResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.client.support.interceptor.ClientInterceptor;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 public class ConsultaRucSoapClient extends WebServiceGatewaySupport {
     private static final Logger log = LoggerFactory.getLogger(ConsultaRucSoapClient.class);
+//    private String usuario= "user";
+//    private String clave = "password";
+
+//    public ConsultaRucSoapClient() throws Exception {
+//
+//        Wss4jSecurityInterceptor securityInterceptor = new Wss4jSecurityInterceptor();
+//        securityInterceptor.setSecurementActions("UsernameToken");
+//        securityInterceptor.setSecurementUsername(usuario);
+//        securityInterceptor.setSecurementPassword(clave);
+//        securityInterceptor.afterPropertiesSet();
+//
+//        // Set the security interceptor to the WebServiceTemplate
+//        getWebServiceTemplate().setInterceptors(new ClientInterceptor[]{securityInterceptor});
+//
+//    }
+
     public ConsultaRucResponse getConsultaRuc(String ruc){
         ConsultaRucRequest request= new ConsultaRucRequest();
         request.setRucParamentro(ruc);
